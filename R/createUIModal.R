5f0ea75eb51da048f651a5b4439ca997e9a0ba9d
Jordan DeHerrera <deherrera@uber.com>
2017-01-18 18:30:45 -0600

    Added UI Modal Function

--- R/createUIModal.R
+++ R/createUIModal.R
@@ -1,42 +1,35 @@
-#' Create a Modal Based On a Google Slide Presentation
+#' Create a Modal Based on a User Interface
 #'
-#' This function creates a modal based on a Google Presentation.
+#' This function creates a modal based on a Shiny UI.
 #' @param id This is the id of the modal that you are creating.
 #' @param modalTitle This is the title of the modal that appears at the top of the modal.
-#' @param prezSrc This is the URL of the published Google presentation that you want to appear in the modal.
-#' @keywords modal, Google Sheets
+#' @param uiOutputText This can be a tag list or a uiOutput from some other Shiny output.
+#' @keywords modal, UI
 #' @export
 #' @examples
-#' output$postmortemModal <- renderUI({
-#' createPrezModal("sample","Sample Prezentation Modal",
-#'                 "https://docs.google.com/presentation/d/14H24z59wQVTnAg9AKD_bIZ6F3DKnGIy9haBK557XMVo/embed?start=false&loop=false&delayms=3000")
-#' })
+#'   createUIModal("sample-id","Upload file modal",
+#'                 tagList(
+#'                   fileInput("file1", "Load a File:")
+#'                 ))
 
-createPrezModal <- function(id, modalTitle, prezSrc) {
-
+createUIModal <- function(id, modalTitle, uiOutputText) {
+  
   ns <- NS(id)
-
+  
   tags$div(class="modal fade", id=ns("modal"), tabindex="-1", role="dialog", `aria-labelledby`=ns("modelLabel"),
            tagList(
-
+             
              HTML('<div class="modal-dialog" role="document">
                   <div class="modal-content">
                   <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'),
-
+             
              tags$h4(class = "modal-title", id=ns("modelLabel"),modalTitle),
-
+             
              HTML('</div><div class="modal-body">'),
-
-             tags$iframe(id=ns("googleform"),
-                         src=prezSrc,
-                         frameborder="0",
-                         width="100%",
-                         height="400px",
-                         allowfullscreen="true",
-                         mozallowfullscreen="true",
-                         webkitallowfullscreen="true"),
-
+             
+             uiOutputText,
+             
              HTML('</div>
                   <div class="modal-footer">
                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
