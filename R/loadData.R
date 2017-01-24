cfbb5cb8ad90fabe62c822dbae0a3e1c500b3722
Jordan DeHerrera <deherrera@uber.com>
2017-01-24 14:22:03 -0600

    SQL functions

---
+++ R/loadData.R
@@ -0,0 +1,21 @@
+#' Load Data from SQL Connection
+#'
+#' This function returns a data frame that is the output of a SQL Query.
+#' @param sqlString This is the query that should be sent to the SQL database.
+#' @keywords sql, RMySQL
+#' @export
+#' @examples
+#'   dat <- loadData("SELECT * FROM t_customers")
+
+loadData <- function(sqlString) {
+  # Connect to the database
+  db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                  port = options()$mysql$port, user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  # Construct the fetching query
+  query <- sprintf(sqlString, table)
+  # Submit the fetch query and disconnect
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  data
+}
---
+++ R/setSQLOptions.R
@@ -0,0 +1,20 @@
+#' Set SQL Options
+#'
+#' This function sets SQL default options for making database connections
+#' @param host Host location (often times localhost)
+#' @param port Port where the mysql server is open
+#' @param user MySQL username
+#' @param password Password used for login for MySQL server
+#' @keywords sql, RMySQL, mysql
+#' @export
+#' @examples
+#'   setSQLOptions("localhost",3306,"user","password")
+
+setSQLOptions <- function(localhoststr, portno, userstr, passwdstr){
+  options(mysql = list(
+  "host" = localhoststr,
+  "port" = portno,
+  "user" = userstr,
+  "password" = passwdstr
+  ))
+}
