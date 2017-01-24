71bf307b8c6f0d0998f63dfe02e400150417d10f
Jordan DeHerrera <deherrera@uber.com>
2017-01-24 10:19:35 -0600

    Shiny Boots Documentation

---
+++ R/buttonRow.R
@@ -0,0 +1,14 @@
+#' Button Row
+#'
+#' This function creates a row of buttons
+#' @param ... Insert button functions in a tagList {shiny}
+#' @keywords button row, buttons
+#' @export
+#' @examples
+#'   buttonRow(taglist(
+#'   buttonModal(buttonModal("sample-id","ID Form")),
+#'   actionButton("submit","Submit Responses", icon = icon("check"))
+#'   ))
+buttonRow <- function( ...){
+  tags$div(class="btn-group", `role`="group", `aria-label`="Basic example", ...)
+}
