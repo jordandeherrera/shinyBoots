754a5d5824e47dba192981d720425419606da58d
Jordan DeHerrera <deherrera@uber.com>
2017-01-31 17:22:28 -0600

    Removed status function

--- R/box.R
+++ R/box.R
@@ -40,7 +40,7 @@ box <- function(..., title = NULL, footer = NULL, status = NULL, solidHeader = F
   }
   collapseTag <- NULL
   if (collapsible) {
-    buttonStatus <- status %OR% "default"
+    buttonStatus <- "default"
     collapseIcon <- if (collapsed)
       "plus"
     else "minus"
