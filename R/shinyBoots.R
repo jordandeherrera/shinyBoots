5d5866299c2090e7e7af793702c92691b6bd8a71
Jordan DeHerrera <deherrera@uber.com>
2017-03-26 15:13:38 -0500

    Experimenting with updating pkg documentation

--- R/shinyBoots.R
+++ R/shinyBoots.R
@@ -1,10 +1,11 @@
 #' Generate R documentation from inline comments.
 #'
-#' Roxygen2 allows you to write documentation in comment blocks co-located
-#' with code.
+#' This package contains functions to make it easier to create a reactive website
+#' using Twitter Bootstrap and Shiny.  It also contains some functions to make it
+#' easier to connect to local database servers.
 #'
-#' The only function you're likely to need from \pkg{roxygen2} is
-#' \code{\link{roxygenize}}. Otherwise refer to the vignettes to see
-#' how to format the documentation.
+#' Some useful functions in shinyBoots include the following:
+#' \code{\link{actionLink}} - to create a link that will start an action on the server-side
+#' \code{\link{buttonModal}} - to create a button that will open a modal
 "_PACKAGE"
 #> [1] "_PACKAGE"
