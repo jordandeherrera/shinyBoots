de8124a218a7eb688f516242c39b8dd21d75c1a5
Jordan DeHerrera <deherrera@uber.com>
2017-03-28 13:03:16 -0500

    Updated description, namespace, and created a widetoLong function

---
+++ R/wideToLong.R
@@ -0,0 +1,20 @@
+#' Convert Dataframe from Wide to Long
+#'
+#' This function converts a data frame from wide format to long.
+#' @param data This is the data source to be converted
+#' @param colSelections These are the columns (references by column number) that contain the values
+#' @keywords modal, data manipulation
+#' @export
+#' @examples
+#' library(dplyr)
+#' mtcars2 <- mtcars %>% mutate(Car = row.names(mtcars)) %>% select(Car,mpg,cyl,disp)
+#' dat <- wideToLong(mtcars2,c(2:4))
+#' })
+
+wideToLong <- function(data,colSelections){
+  require(tidyr)
+
+  dat <- gather(data, Category, Value, colSelections)
+
+  return(dat)
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ShinyBoots
 
 This package contains functions to make it easier to create a reactive website using Twitter Bootstrap and Shiny.  It also contains some functions to make it easier to connect to local database servers.
+
+The package contains functions that fall into the following general categories: functions to create boostrap elements, functions to create a reactive app with CRUD functions (create, retrieve, update, and delete), functions to create charts and visualizations, functions to handle CSV files in a Shiny app, and functions to manipulate data.
